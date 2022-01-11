74e58eb1dc180e7073191116ad5d3740864f7e21
Harris Sultan <harris_sultan@outlook.com>
2022-01-11 15:54:40 -0500

    commit tree 6db7bf417a5032f900c30b3c60050fd8a8464cc3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+customerName = 'bob'
+const leastFavoriteCustomer  = 'you'
+function Name(){
+return customerName
+}
+
+function upperCaseCustomerName(){
+    return customerName =  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='me'
+}
